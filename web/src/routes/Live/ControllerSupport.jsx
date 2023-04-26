549fe5e6467770a92716b8efe30925fefb21a857
Camwp <camwpedro@gmail.com>
2023-04-26 15:26:40 -0700

    test10

--- web/src/routes/Live/ControllerSupport.jsx
+++ web/src/routes/Live/ControllerSupport.jsx
@@ -1,13 +1,14 @@
+if (navigator.getGamepads) {
+    window.addEventListener("gamepadconnected", function(e) {
+      console.log("Gamepad connected:", e.gamepad);
+      // Add your gamepad logic here
+    });
+  
+    window.addEventListener("gamepaddisconnected", function(e) {
+      console.log("Gamepad disconnected:", e.gamepad);
+    });
+  }
 
   export function handleGamepadInput(gamepad) {
-    if (navigator.getGamepads) {
-      window.addEventListener("gamepadconnected", function(e) {
-        console.log("Gamepad connected:", e.gamepad);
-        // Add your gamepad logic here
-      });
-    
-      window.addEventListener("gamepaddisconnected", function(e) {
-        console.log("Gamepad disconnected:", e.gamepad);
-      });
-    }
+    // your gamepad logic here
   }
