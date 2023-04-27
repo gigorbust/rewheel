b4e237fef40d59bd817214f3e7bba1302b64cbaa
Camwp <camwpedro@gmail.com>
2023-04-27 11:26:55 -0700

    test31

--- web/src/routes/Live/RideBehavior.jsx
+++ web/src/routes/Live/RideBehavior.jsx
@@ -96,7 +96,7 @@ export const RideBehavior = () => {
     });
 
     refreshIntervalID = setInterval(() => {
-      if(gamepadIndex !== undefined && refreshIntervalID !== 0) {
+      if(gamepadIndex !== undefined && remoteEnabled) {
         const myGamepad = navigator.getGamepads()[gamepadIndex];
         console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
         console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
