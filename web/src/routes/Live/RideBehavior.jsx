ff12f95b64fa767a884c4b574ba2beb30fcdacb8
Camwp <camwpedro@gmail.com>
2023-04-26 22:10:35 -0700

    test15

--- web/src/routes/Live/RideBehavior.jsx
+++ web/src/routes/Live/RideBehavior.jsx
@@ -99,12 +99,14 @@ export const RideBehavior = () => {
         const myGamepad = navigator.getGamepads()[gamepadIndex];
         console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
         console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
+        setAngleOffset(myGamepad.axes[1], true);
       }
     }, 100)
   }
   const closeRemoteTilt = () => {
     setAngleOffset(0.0, true)
     showRemoteTilt(false)
+    gamepadIndex == undefined;
     
   }
 
