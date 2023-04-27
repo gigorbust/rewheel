e6f22811fa42c05a78338132d1a08175bd35641d
Camwp <camwpedro@gmail.com>
2023-04-27 11:50:32 -0700

    37

--- web/src/routes/Live/RideBehavior.jsx
+++ web/src/routes/Live/RideBehavior.jsx
@@ -101,10 +101,9 @@ export const RideBehavior = () => {
         //console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
         //console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
         setAngleOffset(-(((myGamepad.axes[1]) * 30 )/ 10), true);
-      }
-      else {
-        //clearInterval(refreshIntervalID);
-        console.log("disabled.");
+        if (myGamepad == null) {
+          clearInterval(refreshIntervalID);
+        }
       }
       
     }, 100)
