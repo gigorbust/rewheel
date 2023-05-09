d1fa53a618e5f822e048da9b39a8879e3edebec0
CWP <77127867+Camwp@users.noreply.github.com>
2023-05-08 21:44:20 -0700

    resources links

---
+++ common/resources.js
@@ -0,0 +1,14 @@
+export const resources = {
+  Source1: {
+    "sourceLink": "https://ud.me/0xsundaii.crypto"
+  },
+  Source2: {
+    "sourceLink": "https://ud.me/0xsundaii.crypto"
+  },
+  Source3: {
+    "sourceLink": "https://ud.me/0xsundaii.crypto"
+  },
+  Source4: {
+    "sourceLink": "https://ud.me/0xsundaii.crypto"
+  }
+}
