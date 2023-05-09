5d02e06490727aadc2156cbab18fc2d3b55012e2
CWP <77127867+Camwp@users.noreply.github.com>
2023-05-09 09:03:55 -0700

    Update resources.js

--- common/resources.js
+++ common/resources.js
@@ -5,10 +5,7 @@ export const resources = {
   "Shadowwheel(Local version)": {
     "sourceLink": "https://mega.nz/file/SRpSHAIC"
   },
-  "ReWheel Discord": {
-    "sourceLink": "https://discord.gg/RA92S5Tx6g"
-  },
-  "FixMyPev Discord": {
-    "sourceLink": "https://discord.gg/2TT7PTfdhC"
+  "OTA Files":{
+    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip"
   }
 }
