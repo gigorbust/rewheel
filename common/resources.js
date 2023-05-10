630aa855684999c9c4ef9b4461517a64b0fe7511
CWP <77127867+Camwp@users.noreply.github.com>
2023-05-09 19:19:20 -0700

    Update resources.js

--- common/resources.js
+++ common/resources.js
@@ -6,6 +6,6 @@ export const resources = {
     "sourceLink": "https://mega.nz/file/SRpSHAIC"
   },
   "OTA Files":{
-    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip"
+    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip | Password:PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   }
 }
