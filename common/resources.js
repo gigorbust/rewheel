fbd766b8dd90da54f93d4cfa612fee2c1fe99944
CWP <77127867+Camwp@users.noreply.github.com>
2023-05-09 20:26:49 -0700

    Update resources.js

--- common/resources.js
+++ common/resources.js
@@ -3,9 +3,9 @@ export const resources = {
     "sourceLink": "https://www.fixmypev.com/diy/onewheel/rewheel/"
   },
   "Shadowwheel(Local version)": {
-    "sourceLink": "https://mega.nz/file/SRpSHAIC"
+    "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
   "OTA Files":{
-    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip | Password:PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
+    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip"
   }
 }
