688687a7d679a9d6a61997cafb399bf92cee35b5
Camwp <camwpedro@gmail.com>
2023-05-10 17:09:50 -0700

    textchange

--- common/resources.js
+++ common/resources.js
@@ -5,7 +5,4 @@ export const resources = {
   "Shadowwheel(Local version)": {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
-  "OTA Files":{
-    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip"
-  }
 }
