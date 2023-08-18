41b764f622009d0c6a53b9e0169ea2d045016a0f
Camwp <camwpedro@gmail.com>
2023-08-17 17:32:39 -0700

    newfirmware

--- common/resources.js
+++ common/resources.js
@@ -5,7 +5,7 @@ export const resources = {
   "Shadowwheel(Local version)": {
     "sourceLink": "https://mega.nz/file/SRpSHAIC#PJ1S_X4yEgzXc4IIQi9DZvHO8WqyCj6uBu_OC6Y_p8I"
   },
-  "OTA Files Pint / GT": {
-    "sourceLink": "https://anonfiles.com/ybC82fn5z6/GT_Pint_Firmware_1_zip"
+  "OTA Files Pint / GT & Firmware": {
+    "sourceLink": "https://bafybeifllzsk35d5skjxjh3wm24cu3ngevtcxc3xstxudcncm6wer6q3y4.ipfs.w3s.link/"
   },
 }
